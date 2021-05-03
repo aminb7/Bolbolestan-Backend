d4c79c6d12415a6c9fdc2e772b72deae5944f66e
Mohammad Amin Baqershahi <m.baqershahi@gmail.com>
2021-05-04 03:18:32 +0430

    Fill GradedCourses and Prerequisites tables.

--- src/main/java/com/IE_CA/CA5/repository/BolbolestanRepository.java
+++ src/main/java/com/IE_CA/CA5/repository/BolbolestanRepository.java
@@ -1,6 +1,7 @@
 package com.IE_CA.CA5.repository;
 
 import com.IE_CA.CA5.model.Course;
+import com.IE_CA.CA5.model.GradedCourse;
 import com.IE_CA.CA5.model.Student;
 import com.IE_CA.CA5.utilities.JsonParser;
 import com.IE_CA.CA5.utilities.RawDataCollector;
@@ -9,6 +10,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BolbolestanRepository {
     private static BolbolestanRepository instance;
@@ -44,7 +46,7 @@ public class BolbolestanRepository {
                 "FOREIGN KEY (id) REFERENCES Students(id)," +
                 "FOREIGN KEY (code, classCode) REFERENCES Courses(code, classCode))");
         stmt.addBatch("CREATE TABLE IF NOT EXISTS GradedCourses(id CHAR(100),\ncode CHAR(100),\nclassCode CHAR(100)," +
-                "\nPRIMARY KEY(id, code, classCode)," +
+                "\ngrade FLOAT,\nterm INTEGER,\nPRIMARY KEY(id, code, classCode)," +
                 "FOREIGN KEY (id) REFERENCES Students(id)," +
                 "FOREIGN KEY (code, classCode) REFERENCES Courses(code, classCode))");
         int[] updateCounts = stmt.executeBatch();
@@ -55,8 +57,7 @@ public class BolbolestanRepository {
 
     private void fillTables() throws SQLException {
         fillCourses();
-        fillStudents();
-        fillGradedCourses();
+        fillStudentsAndGradedCourses();
     }
 
     private void fillCourses() throws SQLException {
@@ -69,27 +70,41 @@ public class BolbolestanRepository {
         }
 
         Connection con = ConnectionPool.getConnection();
-        PreparedStatement stmt = con.prepareStatement("INSERT INTO Courses VALUES (?, ?, ?, ?, ?, ?, ?) on duplicate key update code = code");
+        PreparedStatement stmt1 = con.prepareStatement("INSERT INTO Courses VALUES (?, ?, ?, ?, ?, ?, ?) on duplicate key update code = code");
+        PreparedStatement stmt2 = con.prepareStatement("INSERT INTO Prerequisites VALUES (?, ?, ?, ?) on duplicate key update code = code");
         List.of(coursesList).forEach(course -> {
             try {
-                stmt.setString(1, course.getCode());
-                stmt.setString(2, course.getClassCode());
-                stmt.setString(3, course.getName());
-                stmt.setInt(4, course.getUnits());
-                stmt.setString(5, course.getType());
-                stmt.setString(6, course.getInstructor());
-                stmt.setInt(7, course.getCapacity());
-                stmt.addBatch();
+                stmt1.setString(1, course.getCode());
+                stmt1.setString(2, course.getClassCode());
+                stmt1.setString(3, course.getName());
+                stmt1.setInt(4, course.getUnits());
+                stmt1.setString(5, course.getType());
+                stmt1.setString(6, course.getInstructor());
+                stmt1.setInt(7, course.getCapacity());
+                stmt1.addBatch();
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
+            List.of(course.getPrerequisites()).forEach(prerequisite -> {
+                try {
+                    stmt2.setString(1,course.getCode());
+                    stmt2.setString(2,course.getClassCode());
+                    stmt2.setString(3,prerequisite);
+                    stmt2.setString(4,"1");
+                    stmt2.addBatch();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            });
         });
-        int[] updateCounts = stmt.executeBatch();
-        stmt.close();
+        int[] result1 = stmt1.executeBatch();
+        stmt1.close();
+        int[] result2 = stmt2.executeBatch();
+        stmt2.close();
         con.close();
     }
 
-    private void fillStudents() throws SQLException {
+    private void fillStudentsAndGradedCourses() throws SQLException {
         String host = "http://138.197.181.131:5100";
         Student[] studentsList = null;
         try {
@@ -98,33 +113,51 @@ public class BolbolestanRepository {
         catch (Exception e){
             e.printStackTrace();
         }
+        List<String> studentIds = new ArrayList<>();
 
         Connection con = ConnectionPool.getConnection();
-        PreparedStatement stmt = con.prepareStatement("INSERT INTO Students VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on duplicate key update id = id");
+        PreparedStatement stmt1 = con.prepareStatement("INSERT INTO Students VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on duplicate key update id = id;");
         List.of(studentsList).forEach(student -> {
+            studentIds.add(student.getId());
             try {
-                stmt.setString(1, student.getId());
-                stmt.setString(2, student.getName());
-                stmt.setString(3, student.getSecondName());
-                stmt.setString(4, student.getEmail());
-                stmt.setString(5, student.getPassword());
-                stmt.setString(6, student.getBirthDate());
-                stmt.setString(7, student.getField());
-                stmt.setString(8, student.getFaculty());
-                stmt.setString(9, student.getLevel());
-                stmt.setString(10, student.getStatus());
-                stmt.setString(11, student.getImg());
-                stmt.addBatch();
+                stmt1.setString(1, student.getId());
+                stmt1.setString(2, student.getName());
+                stmt1.setString(3, student.getSecondName());
+                stmt1.setString(4, student.getEmail());
+                stmt1.setString(5, student.getPassword());
+                stmt1.setString(6, student.getBirthDate());
+                stmt1.setString(7, student.getField());
+                stmt1.setString(8, student.getFaculty());
+                stmt1.setString(9, student.getLevel());
+                stmt1.setString(10, student.getStatus());
+                stmt1.setString(11, student.getImg());
+                stmt1.addBatch();
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
         });
-        int[] updateCounts = stmt.executeBatch();
-        stmt.close();
+        int[] result1 = stmt1.executeBatch();
+        stmt1.close();
+
+        PreparedStatement stmt2 = con.prepareStatement("INSERT INTO GradedCourses VALUES (?, ?, ?, ?, ?) on duplicate key update id = id");
+        try {
+            Map<String, String> rawGrades = RawDataCollector.requestGrades(host, studentIds);
+            for (Student student : studentsList) {
+                GradedCourse[] gradedCourses = JsonParser.createObject(rawGrades.get(student.getId()), GradedCourse[].class);
+                for (GradedCourse gradedCourse : gradedCourses) {
+                    stmt2.setString(1, student.getId());
+                    stmt2.setString(2, gradedCourse.getCode());
+                    stmt2.setString(3, "1");
+                    stmt2.setFloat(4, gradedCourse.getGrade());
+                    stmt2.setInt(5, gradedCourse.getTerm());
+                    stmt2.addBatch();
+                }
+            }
+        }
+        catch (Exception e) {
+        }
+        int[] result2 = stmt2.executeBatch();
+        stmt2.close();
         con.close();
-    }
-
-    private void fillGradedCourses() {
-
     }
 }
